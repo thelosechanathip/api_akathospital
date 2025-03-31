b66e58113b853f81498f98dcf46f0a8db5b1ed7f
thelosechanathip <thelosechanathip1996@gmail.com>
2025-03-31 09:56:27 +0700

    edit

--- src/app.js
+++ src/app.js
@@ -174,11 +174,12 @@ function startBlacklistScheduler() {
 startBlacklistScheduler();
 
 app.use('*', (req, res) => {
-  if (req.accepts('html')) {
-    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-  } else {
-    msg(res, 404, "404 Not found!!!!");
-  }
+  // if (req.accepts('html')) {
+  //   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+  // } else {
+  //   msg(res, 404, "404 Not found!!!!");
+  // }
+  msg(res, 404, "404 Not found!!!!");
 });
 
 module.exports = app;
